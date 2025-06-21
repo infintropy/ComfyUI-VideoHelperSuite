80d5ace8fae6a1bfd15feaa53c7283d897352b9e
infintropy <36432437+infintropy@users.noreply.github.com>
2025-06-21 05:30:07 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
-from .videohelpersuite.nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from comfy_vhs.videohelpersuite.nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 import folder_paths
-from .videohelpersuite.server import server
-from .videohelpersuite import documentation
-from .videohelpersuite import latent_preview
+from comfy_vhs.videohelpersuite.server import server
+from comfy_vhs.videohelpersuite import documentation
+from comfy_vhs.videohelpersuite import latent_preview
 
 WEB_DIRECTORY = "./web"
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
